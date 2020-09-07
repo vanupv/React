04c7ab9821e42516d3bb4aacc80f15f8d0d134fe
Anup Varma <anupvarma92@gmail.com>
2020-09-07 11:05:50 +0530

    Updates

---
+++ precache-manifest.65db89997590fa29d11c07e1c8aa1c56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d94f7887623bef18471112ba2ceff1",
+    "url": "/vanupv/React/index.html"
+  },
+  {
+    "revision": "e634a16c45feb92cbc56",
+    "url": "/vanupv/React/static/css/main.521886c9.chunk.css"
+  },
+  {
+    "revision": "7ad469fd8f42e2dddec4",
+    "url": "/vanupv/React/static/js/2.7eedeec2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vanupv/React/static/js/2.7eedeec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e634a16c45feb92cbc56",
+    "url": "/vanupv/React/static/js/main.7c317604.chunk.js"
+  },
+  {
+    "revision": "db8da4922c4e780757d3",
+    "url": "/vanupv/React/static/js/runtime-main.8a5cef58.js"
+  }
+]);
